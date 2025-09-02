e321c9610e764ebc439703984f4f5eb8dbbe1d3b
Belen Ruiz Díaz <ruizdiazb378@gmail.com>
2025-09-02 16:42:34 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-import express from 'express';
-import registerRoutes from './routes/index.js';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import registerRoutes from "./routes/index.js";
 
 const app = express();
 const PORT = 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Middleware para JSON
 app.use(express.json());
+
+// Servir archivos estáticos desde /public
+app.use(express.static(path.join(__dirname, "public")));
 
 // Centraliza todas las rutas
 registerRoutes(app);
 
 // Ruta raíz
-app.get('/', (req, res) => {
-  res.json({ mensaje: 'Bienvenido a la API de TechParts' });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 // Iniciar server
