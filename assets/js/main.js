328cdda3cea0c9edfcf452a9eab41f619e02da13
valorite2 <dylanore2005@gmail.com>
2025-09-18 02:34:12 +0200

    Delete assets/js directory

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-const validRoutes = ['dashboard.html', 'inventario.html', 'movimientos.html', 'reportes.html'];
-
-const currentPath = window.location.pathname.split('/').pop();
-
-if (!validRoutes.includes(currentPath)) {
-  window.location.href = '../error/404.html';
-}
