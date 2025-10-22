255c9675e748605cb94dcea96d37094fb69571ad
valorite2 <dylanore2005@gmail.com>
2025-10-22 17:16:03 -0300

    Update reportes.js

--- frontend/dashboard/js/reportes.js
+++ frontend/dashboard/js/reportes.js
@@ -24,120 +24,221 @@ document.addEventListener('click', (event) => {
 
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------
-document.addEventListener('DOMContentLoaded', async () => {
-  // Traer todas las categorías
-  const { data: productos, error } = await supabase
-    .from('products')
-    .select('category');
+// datos traidos de supabase
+const ctx = document.getElementById('lineChart').getContext('2d');
+const { data, error } = await supabase
+  .from('movements')
+  .select('*');
+if (error) {
+  throw new Error('Error al obtener los movimientos: ' + error.message);
+}
 
-  if (error) {
-    console.error('Error al traer productos:', error);
-    return;
-  }
+// Mostrar por defecto la tendencia del mes actual
+const chartData = getChartData("esteMes", data);
+renderChart(chartData);
 
-  // Contar productos por categoría
-  const conteoCategorias = {};
-  productos.forEach(p => {
-    if (conteoCategorias[p.category]) {
-      conteoCategorias[p.category]++;
-    } else {
-      conteoCategorias[p.category] = 1;
+function getChartData(tipo, data) {
+  // Nombres abreviados de los meses
+  const NombreMeses = ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"];
+  const entradasMap = new Map();
+  const salidasMap = new Map();
+  const TextTendencias = document.getElementById('TextTendencias');
+  data.forEach(movimiento => {
+    const fecha = new Date(movimiento.created_at);
+    const mesIndex = fecha.getMonth();
+    const mes = NombreMeses[mesIndex];
+    const dia = fecha.getDate();
+    const año = fecha.getFullYear();
+    //traemos la fecha actual
+    const semana = ObtenerSemanaDelDia(año, mesIndex, dia);
+    // Agrupar cantidades por mes y tipo
+    if (movimiento.type === "entrada") {
+      if (!entradasMap.has(mes)) {
+        const semanasArray = TraerSemanasDeMes(año, mesIndex);
+        const semanasMap = new Map();
+        semanasArray.forEach(semana => {
+          semanasMap.set(semana, []);
+        });
+        entradasMap.set(mes, semanasMap);
+      }
+      entradasMap.get(mes).get(semana).push(movimiento.quantity);
+
+    } else if (movimiento.type === "salida") {
+      if (!salidasMap.has(mes)) {
+        const semanasArray = TraerSemanasDeMes(año, mesIndex);
+        const semanasMap = new Map();
+        semanasArray.forEach(semana => {
+          semanasMap.set(semana, []);
+        });
+        salidasMap.set(mes, semanasMap);
+      }
+      salidasMap.get(mes).get(semana).push(movimiento.quantity);
     }
   });
+  console.log(entradasMap);
+  console.log(salidasMap);
 
-  // Obtener labels y series dinámicamente
-  const labels = Object.keys(conteoCategorias);
-  const series = Object.values(conteoCategorias);
 
-  // Configuración del gráfico
-  const chartOptions = {
-    series: series,
-    labels: labels,
-    colors: ["#10b981", "#3b82f6", "#8b5cf6", "#E74694", "#fcd34d", "#f97316","#f23","#123"],
-    chart: {
-      height: 320,
-      width: "100%",
-      type: "donut",
-    },
-    stroke: { colors: ["transparent"] },
-    plotOptions: {
-      pie: {
-        donut: {
-          labels: {
-            show: true,
-            name: { show: true, fontFamily: "Inter, sans-serif", offsetY: 20 },
-            total: {
-              showAlways: true,
-              show: true,
-              label: "Total productos",
-              fontFamily: "Inter, sans-serif",
-              formatter: function (w) {
-                const sum = w.globals.seriesTotals.reduce((a, b) => a + b, 0);
-                return sum;
-              }
-            },
-            value: {
-              show: true,
-              fontFamily: "Inter, sans-serif",
-              offsetY: -20,
-              formatter: (value) => value
-            }
-          },
-          size: "80%"
-        }
+  // Promediar las cantidades por mes
+  let salidas = [];
+  let entradas = [];
+  let labels = [];
+
+  const fechaActual = new Date();
+  const mesActual = fechaActual.getMonth();
+  const añoActual = fechaActual.getFullYear();
+  const diaActual = fechaActual.getDate();
+  console.log(TraerSemanasDeMes(añoActual, mesActual, diaActual));
+  switch (tipo) {
+    //filtro de la lógica si el data-value es EsteMes.
+    case "esteMes": {
+      //agarramos el mes actual dentro del array de meses
+      const semanaDelDia = ObtenerSemanaDelDia(añoActual, mesActual, diaActual);
+      const semanasDelMes = TraerSemanasDeMes(añoActual, mesActual);
+      const NombreMes = NombreMeses[mesActual];
+      //le pasamos al labels el mes actual
+      console.log(semanasDelMes);
+      labels = semanasDelMes;
+      //le pasamos al array de entradas y salidas la suma total de las entradas y salidas del mes actual
+      entradas = semanasDelMes.map(semana => {
+        const valores = entradasMap.get(NombreMes)?.get(semana);
+        return valores ? valores.reduce((a, b) => a + b, 0) : 0;
+      })
+
+      salidas = semanasDelMes.map(semana => {
+        const valores = salidasMap.get(NombreMes)?.get(semana);
+        return valores ? valores.reduce((a, b) => a + b, 0) : 0;
+      })
+      TextTendencias.textContent = `Entradas vs Salidas de ${NombreMes}`;
+    }
+      break;
+    //filtro de la lógica si el data-value es ultMeses.
+    case "ultMeses": {
+      //logíca para últimos meses
+      let indiceDelMesCambiante = 0;
+      const ultimosMeses = [];
+      //obtenemos los últimos 3 meses
+      for (let i = 4; i >= 0; i--) {
+        const mesIndex = (mesActual - i + 12) % 12;
+        ultimosMeses.push(mesIndex);
       }
-    },
-    dataLabels: { enabled: false },
-    legend: { position: "bottom", fontFamily: "Inter, sans-serif" }
-  };
+      //hacemos un map de los últimos meses para obtener los nombres de los meses
+      let labels = []
+      console.log(labels);
+      const ctxF = { ultimosMeses, NombreMeses, añoActual, entradasMap, salidasMap, labels: [], entradas: [], salidas: [] };
+      renderMes(indiceDelMesCambiante, ctxF);
+      renderChart(ctxF);
+      //recorremos los labels para obtener las entradas y salidas de cada mes
+      const len = ctxF.ultimosMeses.length;
+        TextTendencias.textContent = `Entradas vs Salidas de ${ctxF.NombreMeses[ctxF.ultimosMeses[indiceDelMesCambiante]]}`;
+      
+      document.getElementById('prevMes').addEventListener('click', () => {
+        indiceDelMesCambiante = (indiceDelMesCambiante + 1) % len;
+        renderMes(indiceDelMesCambiante, ctxF);
+        TextTendencias.textContent = `Entradas vs Salidas de ${ctxF.NombreMeses[ctxF.ultimosMeses[indiceDelMesCambiante]]}`;
+        renderChart(ctxF);
 
-  // Renderizar el gráfico
-  if (document.getElementById("donut-chart")) {
-    const chart = new ApexCharts(document.getElementById("donut-chart"), chartOptions);
-    chart.render();
+      })
+      document.getElementById('nextMes').addEventListener('click', () => {
+        indiceDelMesCambiante = (indiceDelMesCambiante - 1 + 5) % len;
+        renderMes(indiceDelMesCambiante, ctxF);
+        TextTendencias.textContent = `Entradas vs Salidas de ${ctxF.NombreMeses[ctxF.ultimosMeses[indiceDelMesCambiante]]}`;
+        renderChart(ctxF);
+
+      })
+      console.log(ctxF.labels);
+
+    }
+      break;
+    //filtro de la lógica si el data-value es porA.
+    case "porA": {
+      //logíca para este año
+      //le pasamos todos los meses al labels
+      labels = NombreMeses;
+      //recorremos los labels para obtener las entradas y salidas de cada mes
+      labels.forEach(mes => {
+        if (entradasMap.has(mes)) {
+          let total = 0;
+          entradasMap.get(mes).forEach(valores => {
+            total += valores.reduce((a, b) => a + b, 0);
+          });
+          entradas.push(total);
+        } else {
+          entradas.push(null);
+        }
+        if (salidasMap.has(mes)) {
+          let total = 0;
+          salidasMap.get(mes).forEach(valores => {
+            total += valores.reduce((a, b) => a + b, 0);
+          });
+          salidas.push(total);
+        } else {
+          salidas.push(null);
+        }
+      })
+      TextTendencias.textContent = `Entradas vs Salidas del año ${añoActual}`;
+    }
+      break;
+         
   }
-});
-
-
-//-------------------------------------------------------------------------------------------------------------------------------------------------------
-
-// GRÁFICO DE LINEAS
-const ctx = document.getElementById('lineChart').getContext('2d');
-const {data,error} = await supabase
-.from('movements')
-.select('*'); 
-if(error){
-throw new Error('Error al obtener los movimientos: ' + error.message);
+  return { labels, entradas, salidas };
 }
-const NombreMeses = [  "Ene", "Feb", "Mar", "Abr", "May", "Jun","Jul", "Ago", "Sep", "Oct", "Nov", "Dic"];
-const entradasMap = new Map(); 
-const salidasMap = new Map();
-data.forEach(movimiento => {
-  const fecha = new Date(movimiento.created_at);
-  const mes = NombreMeses[fecha.getMonth()]; 
 
-  if (movimiento.type === "entrada") {
-    if(!entradasMap.has(mes)){
-      entradasMap.set(mes, []);
-    }
-    entradasMap.get(mes).push(movimiento.quantity);
-  } else if (movimiento.type === "salida") {
-    if(!salidasMap.has(mes)){
-      salidasMap.set(mes, []);
-    }
-    salidasMap.get(mes).push(movimiento.quantity);
-}})
-// Promediar las cantidades por mes
-const salidas = [];
-const entradas = [];
-const labels = NombreMeses;
+function ObtenerSemanaDelDia(año, mes, dia) {
+  const primerDia = new Date(año, mes, 1);
+  const PrimerDiaDe0 = primerDia.getDay();
+  const primerDiaF = PrimerDiaDe0 ? PrimerDiaDe0 - 1 : 6;
 
-labels.forEach(mes => { 
-salidas.push(salidasMap.has(mes) ? salidasMap.get(mes).reduce((a, b) => a + b, 0) : null);
-entradas.push(entradasMap.has(mes) ? entradasMap.get(mes).reduce((a, b) => a + b, 0) : null); 
-})
-console.log(salidasMap.get('Ene'));
-  new Chart(ctx, {
+  const semanaNum = Math.ceil((dia + primerDiaF) / 7);
+  return `Semana ${semanaNum}`;
+}
+function TraerSemanasDeMes(año, mes) {
+
+  const ultimoDia = new Date(año, mes + 1, 0).getDate();
+  const primerDia = new Date(año, mes, 1);
+  const PrimerDiaDe0 = primerDia.getDay();
+  const primerDiaF = PrimerDiaDe0 ? PrimerDiaDe0 - 1 : 6;
+
+  const semanasTotales = Math.ceil((ultimoDia + primerDiaF) / 7);
+
+  const semanas = [];
+  for (let i = 1; i <= semanasTotales; i++) {
+    semanas.push(`Semana ${i}`);
+  }
+
+  return semanas;
+}
+function renderMes(indexMes, ctxF) {
+  // Nombres abreviados de los meses
+  const { ultimosMeses, NombreMeses, añoActual, entradasMap, salidasMap } = ctxF;
+
+  const mesIndex = ultimosMeses[indexMes];
+  const nombreMes = NombreMeses[mesIndex];
+  const semanasDelMes = TraerSemanasDeMes(añoActual, mesIndex);
+
+  ctxF.labels = semanasDelMes
+
+  ctxF.entradas = semanasDelMes.map(semana => {
+    const valores = entradasMap.get(nombreMes)?.get(semana);
+    return valores ? valores.reduce((a, b) => a + b, 0) : 0;
+  })
+
+  ctxF.salidas = semanasDelMes.map(semana => {
+    const valores = salidasMap.get(nombreMes)?.get(semana);
+    return valores ? valores.reduce((a, b) => a + b, 0) : 0;
+  });
+
+  document.getElementById('mesesCambiantes').innerHTML = `${nombreMes}`;
+  
+}
+
+function renderChart({ labels, entradas, salidas }) {
+  if (chartt) {
+    chartt.destroy();
+  }
+
+
+  chartt = new Chart(ctx, {
     type: 'line',
     data: {
       labels: labels,
@@ -170,7 +271,89 @@ console.log(salidasMap.get('Ene'));
         y: { ticks: { color: '#374151' } },
       },
     },
-  }); 
+  })
+};
+
+async function checkSession() {
+  const { data, error } = await supabase.auth.getSession();
+  if (!data.session) {
+    window.location.href = 'session/login.html';
+  } else {
+    // Muestra el nombre de usuario en el dashboard
+    const username = data.session.user.user_metadata?.username || data.session.user.email;
+    document.getElementById('username').textContent = `Bienvenido, ${username}`;
+  }
+}
+checkSession();
+
+async function cuadrosReport() {
+  // Elementos del DOM
+  const valor = document.getElementById('valorT');
+  const sinStock = document.getElementById('sinStock');
+  const alertas = document.getElementById('alertas'); 
+  const ganacias = document.getElementById('ganancias');
+  const valorP = document.getElementById('porcentajeV');     
+  const stock = document.getElementById('div-stock'); 
+  const gananciasP = document.getElementById('gananciasP');
+  // Obtener datos de Supabase para Valor total del mes
+  const {data,error} = await supabase
+  .from('products')
+  .select('*');
+
+  if (error){
+    console.error("Error al obtener los datos:", error);
+    return;
+  }
+  
+const fechaA = new Date();
+const mesA  = fechaA.getMonth();
+let fechaAnterior = mesA - 1;
+if (fechaAnterior < 0) {
+  fechaAnterior = 11; 
+}
+
+let valorTotalM = 0;
+let valorTotalManterior = 0;
+
+data.forEach(producto => {
+  const fechaD = new Date(producto.created_at);
+  const mesD = fechaD.getMonth();
+  
+  const valorProducto = producto.price * producto.stock; // Valor de cada producto
+
+  if (mesD === mesA) {
+    valorTotalM += valorProducto; 
+  }  
+  if (mesD === fechaAnterior) {
+    valorTotalManterior += valorProducto; 
+  }
+});
+
+// Calcular diferencia y porcentaje
+const valorFinal = valorTotalM - valorTotalManterior;
+const porcentaje = valorTotalManterior === 0 ? 0 : (valorFinal / valorTotalManterior) * 100;
+
+// Mostrar resultados
+valor.textContent = `$${valorTotalM.toLocaleString()}`;
+valorP.textContent = porcentaje.toLocaleString() + '% ' + 'vs mes anterior';
+//fin del valor total del mes
+//sin stock
+const filtroStock = data.filter(item => item.stock === 0);
+let sinStockNum = filtroStock.length;
+sinStock.textContent = sinStockNum;
+// Obtener datos de Supabase para las alertas de reportes
+if (error){
+  console.error("Error al obtener los datos:", errorMovimientos);
+  return;
+}
+const Filtro = data.filter(item => item.stock < item.min_stock);
+let alertasNum = Filtro.length;
+alertas.textContent = alertasNum;
+
+
+}
+cuadrosReport();
+
   //-------------------------------------------------------------
 
 const options = {
