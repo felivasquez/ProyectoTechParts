53fb6cad9f86d130e88ce7949fd40b540f003d64
felivasquez <felipevasquezxd@gmail.com>
2025-11-05 20:10:17 -0300

    cambiar ruta de respuesta

--- frontend/tienda/js/paymentForm.js
+++ frontend/tienda/js/paymentForm.js
@@ -52,7 +52,7 @@ async function createPaymentIntent() {
     try {
         const saveCard = saveCardCheckbox.checked;
 
-        const response = await fetch("/backend/create-payment-intent", {
+        const response = await fetch("backend/create-payment-intent", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
