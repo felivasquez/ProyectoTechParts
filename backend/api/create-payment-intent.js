a8cb790d369f28856c2f7954477687bb4f76df92
felivasquez <felipevasquezxd@gmail.com>
2025-11-08 00:25:54 -0300

    Try to fix payments

--- backend/api/create-payment-intent.js
+++ backend/api/create-payment-intent.js
@@ -4,11 +4,11 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Método no permitido" });
+    return res.status(405).json({ error: "Method Not Allowed" });
   }
 
   try {
-    const { amount } = req.body; // Monto en centavos
+    const { amount } = req.body;
 
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
@@ -17,7 +17,8 @@ export default async function handler(req, res) {
     });
 
     res.status(200).json({ clientSecret: paymentIntent.client_secret });
-  } catch (err) {
-    res.status(400).json({ error: err.message });
+  } catch (error) {
+    console.error("Stripe error:", error);
+    res.status(500).json({ error: error.message });
   }
 }
