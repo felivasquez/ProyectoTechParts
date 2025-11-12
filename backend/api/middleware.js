f97796d602702342d554c38c7c6477da1409efec
Belulu77 <ruizdiazb378@gmail.com>
2025-11-12 20:52:23 -0300

    ojala ande esta poronga

---
+++ backend/api/middleware.js
@@ -0,0 +1,11 @@
+export default function middleware(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  return next();
+}
